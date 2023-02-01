015db3b8c378cc8131e0de06dfa9d1aae1510680
Bice93 <bianca.cerulo@hotmail.it>
2023-02-01 14:25:37 +0100

    Created abstract Animale class

---
+++ src/org/lessons/java/Animale.java
@@ -0,0 +1,15 @@
+package org.lessons.java;
+
+public abstract class Animale {
+	
+	
+	public void dormi() {
+		System.out.println("Zzzzzzzz");
+	}
+	
+	//Metodi astratti
+	public abstract void verso();
+	
+	public abstract void mangia();
+	
+}
